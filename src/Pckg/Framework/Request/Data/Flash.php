cc91479acadf61e0a1a48f254adfb3765cb2c4d4
Bojan Rajh <bojan.rajh@spletna-postaja.com>
2016-04-06 09:37:41 +0200

    Implemented Flash

--- src/Pckg/Framework/Request/Data/Flash.php
+++ src/Pckg/Framework/Request/Data/Flash.php
@@ -1,6 +1,40 @@
 <?php namespace Pckg\Framework\Request\Data;
 
-class Flash extends Session
+use Pckg\Framework\Helper\Lazy;
+
+class Flash extends Lazy
 {
+
+    public function __construct($_flash = [])
+    {
+        if (empty($_flash)) {
+            $_flash = isset($_SESSION) && isset($_SESSION['Flash'])
+                ? $_SESSION['Flash']
+                : [];
+        }
+
+        parent::__construct($_flash);
+    }
+
+    public function __destruct()
+    {
+        $_SESSION['Flash'] = $this->data;
+    }
+
+    public function set($name, $val)
+    {
+        return $this->__set($name, $val);
+    }
+
+    public function get($name, $delete = true)
+    {
+        $value = parent::get($name);
+
+        if ($delete) {
+            $this->__unset($name);
+        }
+
+        return $value;
+    }
 
 }
