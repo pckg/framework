3e1cd6559d7b4a4853601355c52a07a38140baf2
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-10-02 13:15:02 +0200

    Add utils.nl2br()

--- src/Pckg/Framework/public/js/http.js
+++ src/Pckg/Framework/public/js/http.js
@@ -135,6 +135,11 @@ var utils = {
 
     pushToVue: function (obj) {
         $.extend(true, $vue, obj);
+    },
+
+    nl2br: function (str, is_xhtml) {
+        var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+        return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
     }
 
 };
