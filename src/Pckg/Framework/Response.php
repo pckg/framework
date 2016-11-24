85b671e18b73761797bc65ce01e20510bd01c357
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-11-24 18:12:05 +0100

    Download file

--- src/Pckg/Framework/Response.php
+++ src/Pckg/Framework/Response.php
@@ -383,4 +383,21 @@ class Response
         return $this;
     }
 
+    public function download($file, $filename)
+    {
+        header("Content-Disposition: attachment; filename=" . urlencode($filename));
+        header("Content-Type: application/octet-stream");
+        header("Content-Type: application/download");
+        header("Content-Description: File Transfer");
+        header("Content-Length: " . filesize($file));
+
+        $fp = fopen($file, "r");
+        while (!feof($fp)) {
+            echo fread($fp, 65536);
+            flush();
+        }
+
+        exit;
+    }
+
 }
