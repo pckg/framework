437884445ab8769336678f301da49ee2771c08d3
Bojan Rajh <schtr4jh@schtr4jh.net>
2016-12-16 11:20:15 +0100

    Utils

--- src/Pckg/Framework/public/js/http.js
+++ src/Pckg/Framework/public/js/http.js
@@ -222,6 +222,15 @@ var utils = {
     nl2br: function (str, is_xhtml) {
         var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
         return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+    },
+
+    closeIfIframe: function () {
+        this.sendToParent('popup.close');
+    },
+
+    sendToParent: function (data) {
+        console.log("Sending event", data);
+        parent.postMessage(data, window.location.origin);
     }
 
 };
