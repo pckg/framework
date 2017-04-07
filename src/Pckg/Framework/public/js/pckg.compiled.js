c80754ab13f56c40f600e864dda9f66da8033ad7
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-04-07 13:38:10 +0200

    Compile

--- src/Pckg/Framework/public/js/pckg.compiled.js
+++ src/Pckg/Framework/public/js/pckg.compiled.js
@@ -1,73 +1,109 @@
-/*var Pckg = {};
-Pckg.Collection = class Collection extends Array {
+var Pckg = {
+    Collection: class extends Array {
 
-    static collect(items, type) {
-        var collection = new Pckg.Collection();
+        static collect(items, type) {
+            var collection = new Pckg.Collection();
 
-        $.each(items, function (i, item) {
-            collection.push(new type(item));
-        }.bind(this));
+            $.each(items, function (i, item) {
+                collection.push(new type(item));
+            }.bind(this));
 
-        return collection;
-    }
-};
-Pckg.Database = {};
-Pckg.Database.Record = class Record {
-    constructor(data) {
-        $.each(data, function (key, val) {
-            this[key] = val;
-        }.bind(this));
-    }
+            return collection;
+        }
+    },
+    Database: {
+        Record: class {
+            constructor(data) {
+                /**
+                 * Set default relations.
+                 */
+                var relations = this.getEntity().getRelations();
+                $.each(relations, function (key, setting) {
+                    if (typeof setting == 'object') {
+                        /**
+                         * Relations type is defined by of (class) and/or type (Array/Object)
+                         */
+                        if (setting.type == Array) {
+                            /**
+                             * Array relation
+                             */
+                            data[key] = Pckg.Collection.collect(data[key] || [], setting.of);
+                        } else {
+                            /**
+                             * Object relation
+                             */
+                            data[key] = new setting.of(data[key] || {});
+                        }
+                    } else {
+                        data[key] = data[key] || new value();
+                    }
+                });
 
-    $set(key, val) {
-        $vue.$set(this, key, val);
-    }
+                /**
+                 * Set default fields.
+                 */
+                $.each(this.getEntity().getFields(), function (key, value) {
+                    data[key] = data[key] || new value();
+                });
 
-    getData() {
-        var fields = this.getEntity().getFields();
-        var data = {};
-
-        $.each(fields, function (i, field) {
-            return data[field] = this[field] ? this[field] : null;
-        });
-
-        return data;
-    }
-
-    insert(callback) {
-        var data = this.getData();
-
-        if (typeof callback == 'undefined') {
-            callback = function (data) {
+                /**
+                 * Bind data to object, set getters and setters for vue.
+                 */
+                $.each(data, function (key, val) {
+                    this[key] = val;
+                }.bind(this));
             }
+
+            $set(key, val) {
+                $vue.$set(this, key, val);
+            }
+
+            getData() {
+                var fields = this.getEntity().getFields();
+                var data = {};
+
+                $.each(fields, (field, val) => {
+                    return data[field] = this[field] || val;
+                });
+
+                return data;
+            }
+
+            insert(callback) {
+                var data = this.getData();
+
+                if (typeof callback == 'undefined') {
+                    callback = function (data) {
+                        console.log('auto callback', data);
+                    };
+                }
+
+                http.post(this.getUrl('insert'), data, callback);
+            }
+
+            getEntity() {
+                return new Pckg.Database.Entity();
+            }
+        },
+
+        Entity: class {
+
+            getFields() {
+                return {};
+            }
+
+            getRelations() {
+                return {};
+            }
+
+            getUrl(type, data) {
+                if (type == 'insert') {
+                    return utils.url();
+                }
+
+                console.log('unknown url');
+            }
+
         }
-
-        http.post(this.getUrl('insert'), data, callback);
-    }
-
-    getEntity() {
-        return new Pckg.Database.Entity();
     }
 };
-Pckg.Database.Entity = class Entity {
-    constructor(data) {
-        $.each(data, function (key, val) {
-            this[key] = val;
-        }.bind(this));
-    }
-
-    $set(key, val) {
-        $vue.$set(this, key, val);
-    }
-
-    getFields() {
-        return [];
-    }
-
-    getUrl(type, data) {
-        if (type == 'insert') {
-            return utils.url();
-        }
-    }
-
-};*/
