c6a7b6ad1bf190108d634a99a851ff92f71ea379
Bojan Rajh <schtr4jh@schtr4jh.net>
2017-10-12 02:02:47 +0200

    No shrinkwrap on pull

--- src/Pckg/Framework/Console/Command/PullProject.php
+++ src/Pckg/Framework/Console/Command/PullProject.php
@@ -21,7 +21,7 @@ class PullProject extends Command
         }
 
         if (!$this->option('no-npm')) {
-            $execs[] = 'npm install';
+            $execs[] = 'npm install --no-shrinkwrap';
         }
 
         $this->exec($execs, true, path('root'));
