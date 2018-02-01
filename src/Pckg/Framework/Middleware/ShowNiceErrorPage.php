6aeeb9bdde7dfe643a000c42d341d632e31303eb
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-02-01 02:45:46 +0100

    Error page

--- src/Pckg/Framework/Middleware/ShowNiceErrorPage.php
+++ src/Pckg/Framework/Middleware/ShowNiceErrorPage.php
@@ -1,11 +1,25 @@
 <?php namespace Pckg\Framework\Middleware;
 
+use Throwable;
+
 class ShowNiceErrorPage
 {
 
     public function handle()
     {
-        return response()->setOutput(view('Pckg/Framework:error/default'));
+        try {
+            $output = view('Pckg/Framework:error/default');
+        } catch (Throwable $e) {
+            if (dev()) {
+                throw $e;
+            }
+
+            $output = '<p>Error during displaying error page. Yes, not funny at all ...</p>';
+        }
+
+        if ($output) {
+            return response()->setOutput($output);
+        }
     }
 
 }
