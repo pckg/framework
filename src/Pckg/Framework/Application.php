3f5535c17752ddf41034be319d2dee1ba3774b84
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-02-05 20:29:52 +0100

    Refactor things

--- src/Pckg/Framework/Application.php
+++ src/Pckg/Framework/Application.php
@@ -20,6 +20,26 @@ class Application
         return $this->provider;
     }
 
+    public function initAndRun()
+    {
+        /**
+         * Initialize application.
+         * This will parse config, set localization 'things', estamblish connection to database, initialize and register
+         * routes, set application autoloaders and providers, session, response, request and assets.
+         */
+        measure('Initializing ' . static::class, function() {
+            $this->init(); // 0.37s -> 0.94s / 1.03s = 57%
+        });
+
+        /**
+         * Run applications.
+         * Everything was preset, we need to run command or request and return response.
+         */
+        measure('Running ' . static::class, function() {
+            $this->run();
+        });
+    }
+
     public function init()
     {
         trigger(Application::class . '.initializing', [$this]);
--- src/Pckg/Framework/Environment.php
+++ src/Pckg/Framework/Environment.php
@@ -3,6 +3,7 @@
 namespace Pckg\Framework;
 
 use Pckg\Concept\Context;
+use Pckg\Concept\Reflect;
 use Pckg\Framework\Environment\Command\DefinePaths;
 use Pckg\Framework\Environment\Development;
 use Pckg\Framework\Environment\Production;
@@ -95,6 +96,44 @@ class Environment implements AssetManager
     public function assets()
     {
         return [];
+    }
+
+    /**
+     * @param Helper\Context $context
+     * @param                $appName
+     * @return Application
+     */
+    public function createApplication(\Pckg\Framework\Helper\Context $context, $appName)
+    {
+
+    }
+
+    public function registerAndBindApplication(\Pckg\Framework\Helper\Context $context, $appName) {
+
+        /**
+         * Register active paths.
+         */
+        $this->registerAppPaths($appName);
+
+        /**
+         * Add app src dir to autoloader and template engine.
+         */
+        $context->registerAutoloaders(path('app_src'), $this);
+
+        /**
+         * Now we will be able to create and register application provider.
+         */
+        return $applicationProvider = Reflect::create(ucfirst($appName));
+    }
+
+    public function registerAppPaths($appName)
+    {
+        path('app', path('root') . "app" . path('ds') . strtolower($appName) . path('ds'));
+        path('app_src', path('app') . "src" . path('ds'));
+        path('app_public', path('app') . "public" . path('ds'));
+        path('app_uploads', path('uploads'));
+        path('app_storage', path('storage'));
+        path('app_private', path('private'));
     }
 
 }
