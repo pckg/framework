0fd3e1a7b7e55cd61e1e5915c7dd6d97cfe9e325
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-04-02 15:31:33 +0200

    UUID, error response, URL data, fix splice

--- src/Pckg/Framework/Helper/functions.php
+++ src/Pckg/Framework/Helper/functions.php
@@ -197,6 +197,12 @@ if (!function_exists('auth')) {
     }
 }
 
+if (!function_exists('uuid4')) {
+    function uuid4(){
+        return \Ramsey\Uuid\Uuid::uuid4();
+    }
+}
+
 if (!function_exists('response')) {
     /**
      * @return Response
