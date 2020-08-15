e5b7bdd279dda01383ac6f14efebd30248b15c28
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-08-15 14:46:39 +0200

    Global EncapsulateResponse

---
+++ src/Pckg/Framework/Response/Afterware/EncapsulateResponse.php
@@ -0,0 +1,35 @@
+<?php namespace Pckg\Framework\Response\Afterware;
+
+class EncapsulateResponse
+{
+
+    public function execute(callable $next)
+    {
+        if (!isHttp()) {
+            return $next();
+        }
+
+        $request = request();
+        if (!$request->isGet()) {
+            return $next();
+        }
+        $router = router();
+        $tags = $router->get('tags');
+
+        if (!in_array('layout:frontend', $tags)) {
+            return $next();
+        }
+
+        $response = response();
+        $output = $response->getOutput();
+        if (is_string($output) && substr($output, 0, 1) === '<' && substr($output, 0, strlen('<!DOCTYPE html>')) !== '<!DOCTYPE html>') {
+            $layout = config('pckg.router.layout');
+            if ($layout) {
+                $response->setOutput(view($layout, ['content' => $output])->autoparse());
+            }
+        }
+
+        return $next();
+    }
+
+}
