02002ec9608f03c4a834103e2b9b6db913ef0995
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-03-08 23:05:40 +0100

    Better defaults

--- src/Pckg/Framework/Helper/functions.php
+++ src/Pckg/Framework/Helper/functions.php
@@ -1207,7 +1207,7 @@ if (!function_exists('between')) {
 }
 
 if (!function_exists('route')) {
-    function route($route, $view, $controller = null)
+    function route($route = '', $view = 'index', $controller = null)
     {
         return new Pckg\Framework\Router\Route\Route($route, $view, $controller);
     }
